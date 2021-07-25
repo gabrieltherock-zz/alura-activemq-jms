7b2a874125d74bcf5e72d64f6b91e4315e80bf0f
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-07-25 14:24:01 -0300

    conhecendo o BrowseMessage

--- src/main/java/br/com/gabriel/activemq/TesteConsumidor.java
+++ src/main/java/br/com/gabriel/activemq/TesteConsumidor.java
@@ -5,9 +5,12 @@ import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MessageConsumer;
+import javax.jms.Queue;
+import javax.jms.QueueBrowser;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class TesteConsumidor {
@@ -21,6 +24,7 @@ public class TesteConsumidor {
 
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Destination fila = (Destination) context.lookup("financeiro");
+
         MessageConsumer consumer = session.createConsumer(fila);
 
         consumer.setMessageListener(message -> {
@@ -37,5 +41,16 @@ public class TesteConsumidor {
         session.close();
         connection.close();
         context.close();
+    }
+
+    //não consome a mensagem, apenas avisa que elas estão lá
+    static void browseMessages(Session session, Destination fila) throws Exception {
+        QueueBrowser browser = session.createBrowser((Queue) fila);
+
+        Enumeration msgs = browser.getEnumeration();
+        while (msgs.hasMoreElements()) {
+            TextMessage msg = (TextMessage) msgs.nextElement();
+            System.out.println("Message: " + msg.getText());
+        }
     }
 }
